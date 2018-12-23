e408dc69f3aed176d71068a2011e07d0e5d727f9
Patrick Lee <patrick.lee@u-psud.fr>
2018-12-23 10:22:34 +0100

    get rid of duplicates (m)

--- file_handling.py
+++ file_handling.py
@@ -50,7 +50,7 @@ class FileHandling():
 
     def delete(self):
         """
-        delete row from database
+        delete row and get rid of duplicates from database
         """
         index_touched =  self.touched[self.touched].index
         self.df = self.df.loc[index_touched]
